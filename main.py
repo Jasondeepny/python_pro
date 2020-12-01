16e29088cbf48aaf29daf752c84dc2bb3e27c6f6
zyy <zhouyanyang@zcckj.com>
2020-12-01 22:16:34 +0800

    origin

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print(3%5)
+# print(3 % 5)
+# age = 120
+#
+# if 0 <= age <= 120:
+#     print("正确")
+# else:
+#     print("不正确")
+#
+# holiday_name = "情人节"
+# if holiday_name == "情人节":
+#     print("送玫瑰")
+# elif holiday_name == "平安夜":
+#     print("送苹果")
+# else:
+#     print("送礼物")
+#
+
+has_ticket = True
+knife_length = 30
+
+if has_ticket:
+    print("准备安检")
+    if has_ticket >= 30:
+        print("请将管制刀具放入软件")
+    else:
+        print("进入候车厅")
+else:
+    print("请先买票")
+
+# print(random.randint(0, 100))
